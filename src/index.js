3218ebd4972d099151a3da32e109f578ce61a545
Christian Espinoza <chrs_espi@yahoo.com>
2018-08-13 15:32:43 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import './index.css'
+import Board from './Board'
+
+
+ReactDOM.render(<Board count={50}/>, document.getElementById('root'))
